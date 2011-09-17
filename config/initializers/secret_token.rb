09e05ad9c6d944ae8268e8dad7ab02e831c4f6c4
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2011-09-17 14:09:16 +0900

    initializes rails3 app
    * sqlite3
    * rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nanicollection::Application.config.secret_token = '93616b5ccd22cd04f715d72fbbcf641364eda5c63c6bd88d950393e69e900db2f3690fe34ec3319c4ac6bd017260e2c180f0372b74e864781621788d0eb09c30'
